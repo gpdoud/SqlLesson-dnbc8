a148b448c8fa4bc09ad313423a0dc1339322a14f
Greg Doud <gpdoud@gmail.com>
2020-02-19 16:45:00 -0500

    Add project files.

---
+++ SqlLibrary/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SqlLibrary {
+    
+    public class Student {
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int SAT { get; set; }
+        public double GPA { get; set; }
+        public int MajorId { get; set; }
+
+        private BcConnection bcConnection;
+
+        public List<Student> GetAllStudents() {
+            var sql = "SELECT * From Student";
+            var command = new SqlCommand(sql, bcConnection.Connection);
+            var reader = command.ExecuteReader();
+            if(!reader.HasRows) {
+                Console.WriteLine("No rows from GetAllStudents()");
+                return new List<Student>();
+            }
+            var students = new List<Student>();
+            while(reader.Read()) {
+                var student = new Student();
+                student.Id = Convert.ToInt32(reader["Id"]);
+                student.Firstname = reader["Firstname"].ToString();
+                student.Lastname = reader["Lastname"].ToString();
+                student.SAT = Convert.ToInt32(reader["SAT"]);
+                student.GPA = Convert.ToDouble(reader["GPA"]);
+                //student.MajorId = Convert.ToInt32(reader["MajorId"]);
+                students.Add(student);
+            }
+            return students;
+        }
+
+        public Student() { }
+        public Student(BcConnection connection) {
+            bcConnection = connection;
+        }
+    }
+}
