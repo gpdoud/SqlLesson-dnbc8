146a9bc118dac3ad4de65e58a8ae7994dff6af8f
Greg Doud <gpdoud@gmail.com>
2020-02-20 16:34:26 -0500

    CSharp to SQL for EdDb
    * Completed Student controller
    * Completed Major GetAllMajors method

--- SqlLesson/Program.cs
+++ SqlLesson/Program.cs
@@ -9,8 +9,54 @@ namespace SqlLesson {
 
             var sqllib = new BcConnection();
             sqllib.Connect(@"localhost\sqlexpress", "EdDb", "trusted_connection=true");
-            var student = new Student(sqllib);
-            var students = student.GetAllStudents();
+            
+            MajorController.bcConnection = sqllib;
+
+            var majors = MajorController.GetAllMajors();
+            foreach(var major in majors) {
+                Console.WriteLine(major);
+            }
+
+
+            StudentController.bcConnection = sqllib;
+
+            var students = StudentController.GetAllStudents();
+            foreach(var student0 in students) {
+                Console.WriteLine(student0);
+            }
+
+            //var newStudent = new Student {
+            //    Id = 444,
+            //    Firstname = "Fred",
+            //    Lastname = "Fives",
+            //    SAT = 500,
+            //    GPA = 5.00,
+            //    MajorId = 1
+            //};
+            //var success = StudentController.InsertStudent(newStudent);
+
+
+
+            var student = StudentController.GetStudentByPk(888);
+            if(student == null) {
+                Console.WriteLine("Student not found");
+            } else {
+                Console.WriteLine(student);
+            }
+
+            student.Firstname = "Charlie";
+            student.Lastname = "Chan";
+            var success = StudentController.UpdateStudent(student);
+
+            var studentToDelete = new Student {
+                Id = 999
+            };
+            //success = StudentController.DeleteStudent(999);
+
+            students = StudentController.GetAllStudents();
+            foreach(var student0 in students) {
+                Console.WriteLine(student0);
+            }
             sqllib.Disconnect();
         }
     }
--- SqlLibrary/Student.cs
+++ SqlLibrary/Student.cs
@@ -12,35 +12,14 @@ namespace SqlLibrary {
         public string Lastname { get; set; }
         public int SAT { get; set; }
         public double GPA { get; set; }
-        public int MajorId { get; set; }
+        public int? MajorId { get; set; }
 
-        private BcConnection bcConnection;
+        public Major Major { get; set; }
 
-        public List<Student> GetAllStudents() {
-            var sql = "SELECT * From Student";
-            var command = new SqlCommand(sql, bcConnection.Connection);
-            var reader = command.ExecuteReader();
-            if(!reader.HasRows) {
-                Console.WriteLine("No rows from GetAllStudents()");
-                return new List<Student>();
-            }
-            var students = new List<Student>();
-            while(reader.Read()) {
-                var student = new Student();
-                student.Id = Convert.ToInt32(reader["Id"]);
-                student.Firstname = reader["Firstname"].ToString();
-                student.Lastname = reader["Lastname"].ToString();
-                student.SAT = Convert.ToInt32(reader["SAT"]);
-                student.GPA = Convert.ToDouble(reader["GPA"]);
-                //student.MajorId = Convert.ToInt32(reader["MajorId"]);
-                students.Add(student);
-            }
-            return students;
+        public override string ToString() {
+            return $"{Id}|{Firstname} {Lastname}|{SAT}|{GPA}|{Major?.Description}";
         }
 
         public Student() { }
-        public Student(BcConnection connection) {
-            bcConnection = connection;
-        }
     }
 }
